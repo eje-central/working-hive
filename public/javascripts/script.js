9ab2434e5525f2fe5f4c4799b879352ef02e9a51
noedavidgonzalez <noedabid@gmail.com>
2019-02-10 05:37:24 -0600

    integracion ionicon

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("IronGenerator JS imported successfully!");
+    //console.log("IronGenerator JS imported successfully!");
 
     function getAll(selector) {
       return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
@@ -41,8 +41,8 @@ document.addEventListener(
     });
 
     function deleteUser(id) {
-      restUsersApi
-        .delete(id)
+      axios
+        .delete("/user/delete/"+id)
         .then(responseFromAPI => {
           console.log("Response from API is: ", responseFromAPI.data);
           $(".modal").removeClass("is-active");
@@ -52,7 +52,25 @@ document.addEventListener(
           console.log("Error is: ", err);
         });
     }
-
+    function addUser(){
+      axios
+        .post("/user/add", {
+          username: $("#username").val(),
+          name: $("#name").val(),
+          rol_id: $("#rol").val(),
+          salary: $("#salary").val(),
+          pm: $("#pm").prop("checked")
+        })
+        .then(responseFromAPI => {
+          console.log("Response from API is:", responseFromAPI);
+          $(".modal").removeClass("is-active");
+          $("#modal-guardar").removeClass("is-loading");
+          window.location.href = "/users";
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
     $(".eliminar-usuario").click(function() {
       $(".modal").addClass("is-active");
       $("#modal-guardar").attr("style", "display: none");
@@ -67,6 +85,18 @@ document.addEventListener(
     });
     $("#modal-eliminar").click(function () { 
       deleteUser($("#userToDelete").val()); 
+    });
+
+    $("#btnAgregarUsuario").click( function () {
+      $(".modal").addClass("is-active");
+      $("#modal-guardar").attr("style", "display: block");
+      $("#modal-eliminar").attr("style", "display: none");
+      $(".modal-card-title").html("Agregar nuevo usuario"); 
+      $(".modal-card-body").html($("#nuevoUsuario").html());
+    });
+    $("#modal-guardar").click(function () { 
+      $("#modal-guardar").addClass("is-loading");
+      addUser();
     });
   },
   false
--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,15 @@ authRoutes.get("/users", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   User.find()
     .populate('users')
     .then(users => { 
-      res.render("users", { users, user: req.user });
+      Roles.find()
+        .populate("roles")
+        .then(roles => {
+          res.render("users", { users, user: req.user, roles });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      
     })
     .catch(err => {
       console.log(err)
@@ -29,13 +37,13 @@ authRoutes.get("/user/add", ensureLogin.ensureLoggedIn(), (req, res, next) => {
 });
 
 authRoutes.post("/user/add", ensureLogin.ensureLoggedIn(), (req, res, next) => { 
-  const { mail, name, rol_id, salary, pm } = req.body; 
+  const { username, name, rol_id, salary, pm } = req.body; 
   console.log(req.body);
-  const newUser = new User({ mail, name, rol_id, salary, pm });
+  const newUser = new User({ username, name, password: "",rol_id, salary, pm });
   newUser
     .save()
-    .then(() => {
-      res.redirect(301, "/users");
+    .then(() => { 
+      res.json({ success: username });
     })
     .catch(err => console.log(err)); 
 });
@@ -55,8 +63,7 @@ authRoutes.get("/user/update",
   (req, res, next) => {
     User.find()
       .populate("users")
-      .then(users => {
-        console.log("users:::", users);
+      .then(users => { 
         res.render("users", { users, user: req.user });
       })
       .catch(err => {
