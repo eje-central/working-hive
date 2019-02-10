604bab337debf6acd40cfe212d3f48ddcf95c28d
noedavidgonzalez <noedabid@gmail.com>
2019-02-10 04:01:44 -0600

    eliminar usuarios con modal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,73 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
 
-  console.log('IronGenerator JS imported successfully!');
+    function getAll(selector) {
+      return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+    }
+    function closeModals() {
+      rootEl.classList.remove("is-clipped");
+      $modals.forEach(function ($el) {
+        $el.classList.remove("is-active");
+      });
+    }
+    var rootEl = document.documentElement;
+    var $modals = getAll('.modal');
+    var $modalButtons = getAll('.modal-button');
+    var $modalCloses = getAll(
+      ".modal-background, .modal-close, .modal-card-head, .cancel"
+    );
+    if ($modalCloses.length > 0) {
+      $modalCloses.forEach(function ($el) {
+        $el.addEventListener("click", function () {
+          closeModals();
+        });
+      });
+    }
+    var dropdown = document.querySelector(".dropdown");
 
-}, false);
+    dropdown.addEventListener("click", function(event) {
+      event.stopPropagation();
+      dropdown.classList.toggle("is-active");
+    });
 
-var dropdown = document.querySelector('.dropdown');
+    $("#showModal").click(function() {
+      $(".modal").addClass("is-active");
+    });
+ 
+    const restUsersApi = axios.create({
+      baseURL: "http://localhost:3000/user/delete/"
+    });
 
-dropdown.addEventListener('click', function(event) {
-  event.stopPropagation();
-  dropdown.classList.toggle('is-active');
-});
+    function deleteUser(id) {
+      restUsersApi
+        .delete(id)
+        .then(responseFromAPI => {
+          console.log("Response from API is: ", responseFromAPI.data);
+          $(".modal").removeClass("is-active");
+          window.location.href = "/users";
+        })
+        .catch(err => {
+          console.log("Error is: ", err);
+        });
+    }
 
-$("#showModal").click(function() {
-  $(".modal").addClass("is-active");  
-});
-
-$(".modal-close").click(function() {
-   $(".modal").removeClass("is-active");
-});
+    $(".eliminar-usuario").click(function() {
+      $(".modal").addClass("is-active");
+      $("#modal-guardar").attr("style", "display: none");
+      $("#modal-eliminar").attr("style", "display: block");
+      $(".modal-card-title").html("Eliminar usuario");
+      $(".modal-card-body").html(
+        "El usuario <b>" +
+          $("." + this.id).html() +
+          "</b> será eliminado, ¿Deseas continuar?"
+      );
+      $("#userToDelete").val(this.id);
+    });
+    $("#modal-eliminar").click(function () { 
+      deleteUser($("#userToDelete").val()); 
+    });
+  },
+  false
+);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 
 const User = require("../models/user");
+const Roles = require("../models/roles")
 const express = require("express");
 const authRoutes = express.Router();
 const ensureLogin = require("connect-ensure-login");
@@ -7,8 +8,7 @@ const ensureLogin = require("connect-ensure-login");
 authRoutes.get("/users", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   User.find()
     .populate('users')
-    .then(users => {
-      console.log("users:::", users);
+    .then(users => { 
       res.render("users", { users, user: req.user });
     })
     .catch(err => {
@@ -18,21 +18,36 @@ authRoutes.get("/users", ensureLogin.ensureLoggedIn(), (req, res, next) => {
 );
 
 authRoutes.get("/user/add", ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  res.render("users", { user: req.user , message:"El usuario se agregó correctamente"});
+    Roles.find()
+      .populate("roles")
+      .then(roles => { 
+        res.render("user-add", { user: req.user, roles });
+      })
+      .catch(err => {
+        console.log(err);
+      });
 });
 
-authRoutes.get("/user/delete", 
-  ensureLogin.ensureLoggedIn(), 
-  (req, res, next) => {
-    User.find()
-    .populate("users")
-    .then(users => {
-      console.log("users:::", users);
-      res.render("users", { users, user: req.user });
+authRoutes.post("/user/add", ensureLogin.ensureLoggedIn(), (req, res, next) => { 
+  const { mail, name, rol_id, salary, pm } = req.body; 
+  console.log(req.body);
+  const newUser = new User({ mail, name, rol_id, salary, pm });
+  newUser
+    .save()
+    .then(() => {
+      res.redirect(301, "/users");
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err)); 
+});
+
+authRoutes.delete("/user/delete/:id",
+  (req, res, next) => {
+    let id = req.params.id;
+    User.deleteOne({ _id: id }, function(
+      err,
+      results
+    ) {});
+    res.json({ success: id })
 });
 
 authRoutes.get("/user/update",
